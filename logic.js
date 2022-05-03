13f69f87129863f5233b0e2b3bb4ff3d90fb6acf
Kimpes <gustafssonfilip@hotmail.com>
2022-05-03 10:48:07 +0200

    Render user list from javascript
    You can't add users, but they are at least now rendered from the javascript

--- logic.js
+++ logic.js
@@ -1,3 +1,51 @@
 window.addEventListener("load", onLoadHandler);
 
-function onLoadHandler() {}
+//here I create the user array and fill it with a couple users
+let userListElement;
+let userArray = [
+  {
+    name: "Stella",
+    tasks: [
+      "Feed the dog",
+      "Pet the dog",
+      "Take out the dog",
+      "Harvest the dog",
+    ],
+  },
+  {
+    name: "Bella",
+    tasks: ["Find a dog", "Eat the dog"],
+  },
+  (user3 = {
+    name: "Cruella Devilla",
+    tasks: ["Find a Bella", "Eat the Bella"],
+  }),
+];
+
+function onLoadHandler() {
+  renderUserList();
+  console.log(userListElement);
+}
+
+function renderUserList() {
+  userListElement = document.getElementById("user-selector-grid");
+
+  //fills user list with users
+  for (let user of userArray) {
+    let userElement = document.createElement("div");
+    userElement.classList.add("user");
+    userElement.innerText = user.name;
+    // userElement.addEventListener("click", () => {
+    //     userElement.id.add("selected");
+    // });
+    userListElement.appendChild(userElement);
+  }
+
+  //Remove add user button and readd it to the end
+  let addUserElement = document.getElementById("add-user");
+  console.log(addUserElement);
+  addUserElement.parentNode.removeChild(addUserElement);
+  userListElement.appendChild(addUserElement);
+}
+
+function renderTaskList(user) {}
